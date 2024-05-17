21359711f23e3f569c3f05b54bbc05d9846a8761
Samantha-Varguez <varaguezsamy@gmail.com>
2024-05-17 06:44:56 -0600

    front

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 // 2
 const httpLink = createHttpLink({
-  uri: 'http://localhost:8000/graphql/'
+  uri: 'http://35.197.2.214:8081/graphql/'
 });
 
 const authLink = setContext((_, { headers }) => {
